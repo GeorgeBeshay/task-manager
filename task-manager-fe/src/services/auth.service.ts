ade7374351ce1f01c073fb76dfe9a32d98f684e6
George Beshay <georgebeshay101@gmail.com>
2025-08-24 00:53:52 +0300

    Removing console.log statements.

--- task-manager-fe/src/services/auth.service.ts
+++ task-manager-fe/src/services/auth.service.ts
@@ -6,16 +6,10 @@ const API_URL = 'http://localhost:3000';
 
 export const signUp = async (data: SignUpPayload): Promise<AuthResponse> => {
   const response: AxiosResponse<AuthResponse> = await axios.post(`${API_URL}/auth/signup`, data);
-  // TODO: Remove this
-  console.log("Here is the response from the backend:");
-  console.log(response.data);
   return response.data;
 };
 
 export const signIn = async (data: { email: string; password: string }): Promise<AuthResponse> => {
   const response: AxiosResponse<AuthResponse> = await axios.post(`${API_URL}/auth/login`, data);
-  // TODO: Remove this
-  console.log("Here is the response from the backend:");
-  console.log(response.data);
   return response.data;
 }
