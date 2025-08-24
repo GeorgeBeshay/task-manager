61f89ac977346ab74d223ee868f4b71235ae8304
George Beshay <georgebeshay101@gmail.com>
2025-08-24 12:10:01 +0300

    Finished integrating the different pages and smooth navigation between the pages while maintaining the user state.

---
+++ task-manager-fe/src/pages/Home.tsx
@@ -0,0 +1,39 @@
+import '@/pages/Home.css'
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className="flex flex-col justify-center p-6 space-y-8">
+
+      <div className="flex flex-col p-4 mb-8">
+        <h1 className="text-3xl italic mb-6">
+          <span className='font-bold'>Sch</span>edy
+        </h1>
+        <p className="text-lg mb-2">Welcome to Schedy, your personal task manager.</p>
+      </div>
+
+      <div className="mb-8">
+        <p className="mb-4">Get started by creating your account.</p>
+        <Link
+          to="/signup"
+          className="px-4 py-2 rounded-lg border hover:bg-gray-100 transition"
+        >
+          Sign Up
+        </Link>
+      </div>
+
+      <div>
+        <p className="text-sm mb-4">Already having an account?</p>
+        <Link
+          to="/login"
+          className="px-4 py-2 rounded-lg border hover:bg-gray-100 transition"
+        >
+          Log In
+        </Link>
+      </div>
+    </div>
+  );
+
+}
+
+export default Home
